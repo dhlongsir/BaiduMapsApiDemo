9b21a71a9e780d7241cbb78042cb7f5c0122608d
dhlongsir <2405902561@qq.com>
2017-04-19 08:36:17 +0800

    导入
    没什么

---
+++ src/com/example/wechatsample/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.wechatsample;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+ 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import baidumapsdk.demo.BMapApiDemoMain;
+
+import com.example.wechatsample.R;
+import com.astuetz.PagerSlidingTabStrip;
+
+/**
+ * �߷�΢�ŵ�������
+ * 
+ * http://blog.csdn.net/guolin_blog/article/details/26365683
+ * 
+ * @author guolin
+ */
+public class MainActivity extends FragmentActivity {
+
+	/**
+	 * ��������Fragment
+	 */
+	private ChatFragment chatFragment;
+	private Button plus;
+	/**
+	 * ���ֽ����Fragment
+	 */
+	private FoundFragment foundFragment;
+
+	/**
+	 * ͨѶ¼�����Fragment
+	 */
+	private ContactsFragment contactsFragment;
+
+	/**
+	 * PagerSlidingTabStrip��ʵ��
+	 */
+	private PagerSlidingTabStrip tabs;
+
+	/**
+	 * ��ȡ��ǰ��Ļ���ܶ�
+	 */
+	private DisplayMetrics dm;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setOverflowShowingAlways();
+		dm = getResources().getDisplayMetrics();
+		ViewPager pager = (ViewPager) findViewById(R.id.pager);
+		tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+		pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+		tabs.setViewPager(pager);
+		setTabsValue();
+		plus = (Button)findViewById(R.id.plus);
+		plus.setOnClickListener(new Listener());
+	}
+
+	/**
+	 * ��PagerSlidingTabStrip�ĸ������Խ��и�ֵ��
+	 */
+	private void setTabsValue() {
+		// ����Tab���Զ��������Ļ��
+		tabs.setShouldExpand(true);
+		// ����Tab�ķָ�����͸����
+		tabs.setDividerColor(Color.TRANSPARENT);
+		// ����Tab�ײ��ߵĸ߶�
+		tabs.setUnderlineHeight((int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, 1, dm));
+		// ����Tab Indicator�ĸ߶�
+		tabs.setIndicatorHeight((int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, 4, dm));
+		// ����Tab�������ֵĴ�С
+		tabs.setTextSize((int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_SP, 16, dm));
+		// ����Tab Indicator����ɫ
+		tabs.setIndicatorColor(Color.parseColor("#45c01a"));
+		// ����ѡ��Tab���ֵ���ɫ (�������Զ����һ������)
+		tabs.setSelectedTextColor(Color.parseColor("#45c01a"));
+		// ȡ�����Tabʱ�ı���ɫ
+		tabs.setTabBackground(0);
+	}
+
+	public class MyPagerAdapter extends FragmentPagerAdapter {
+
+		public MyPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		private final String[] titles = { "个人主页", "地图", "朋友圈" };
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return titles[position];
+		}
+
+		@Override
+		public int getCount() {
+			return titles.length;
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			switch (position) {
+			case 0:
+				if (chatFragment == null) {
+					chatFragment = new ChatFragment();
+				}
+				return chatFragment;
+			case 1:
+				if (foundFragment == null) {
+					foundFragment = new FoundFragment();
+				}
+				return foundFragment;
+			case 2:
+				if (contactsFragment == null) {
+					contactsFragment = new ContactsFragment();
+				}
+				return contactsFragment;
+			default:
+				return null;
+			}
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onMenuOpened(int featureId, Menu menu) {
+		if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+			if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+				try {
+					Method m = menu.getClass().getDeclaredMethod(
+							"setOptionalIconsVisible", Boolean.TYPE);
+					m.setAccessible(true);
+					m.invoke(menu, true);
+				} catch (Exception e) {
+				}
+			}
+		}
+		return super.onMenuOpened(featureId, menu);
+	}
+
+	private void setOverflowShowingAlways() {
+		try {
+			ViewConfiguration config = ViewConfiguration.get(this);
+			Field menuKeyField = ViewConfiguration.class
+					.getDeclaredField("sHasPermanentMenuKey");
+			menuKeyField.setAccessible(true);
+			menuKeyField.setBoolean(config, false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	class  Listener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			//取得两个EditText控件的值 
+			//将这两个值存放到Intent对象当中
+			Intent intent = new Intent();
+			intent.setClass(MainActivity.this, BMapApiDemoMain.class);
+			//使用这个Intent对象来启动ResultActivity
+			MainActivity.this.startActivity(intent);
+		}
+	}
+	
+	
+}
+
